cec5888c074f5caf49f25d4f1d6df1ad3bf3ff06
HoltChas <xinghaocong@xzgoddess.com>
2022-04-06 18:11:10 +0800

    Initial commit

---
+++ app/src/main/java/com/holtchas/wanandroid/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.holtchas.wanandroid.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.holtchas.wanandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
